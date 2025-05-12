909e1667f38c864e02b06172275fa191e132f609
Dennis Liu <dennis48161025@gmail.com>
2025-05-12 21:55:56 +0800

    fix: use select to make program stop automatically
    In previous version, getline can't be stopped without an
    interrupt. Hence, select being used and when the program stopped
    by the user, NonBlockingInput will send '\n' into pipe to wake up
    select.
    
    Signed-off-by: Dennis Liu <dennis48161025@gmail.com>

--- test/usb_test/inc/host_usb_lib/userInput/non_blocking_input.hpp
+++ test/usb_test/inc/host_usb_lib/userInput/non_blocking_input.hpp
@@ -35,18 +35,25 @@ class NonBlockingInput {
  public:
   NonBlockingInput()
       : uiparser_([this]() { return PopFirst(); }), exit_flag_(false) {
+    if (pipe(pipefd_) == -1) {
+      throw std::runtime_error("pipe creation failed");
+    }
     input_thread_ = std::thread(&NonBlockingInput::InputLoop, this);
   }
 
   ~NonBlockingInput() {
     exit_flag_.store(true);
 
-    // force std::cin get EOF just now (input thread)
-    ::close(STDIN_FILENO);
+    // notify InputLoop by writing to the pipe
+    char buf = '\n';
+    ::write(pipefd_[1], &buf, 1);
 
     if (input_thread_.joinable()) {
       input_thread_.join();
     }
+
+    ::close(pipefd_[0]);
+    ::close(pipefd_[1]);
   }
 
   // XXX: It's for uncomplete CLI interface
@@ -56,7 +63,7 @@ class NonBlockingInput {
         lock, [this]() { return !input_queue_.empty() || exit_flag_.load(); });
 
     if (input_queue_.empty()) return {""};
-    std::string input = input_queue_.front();
+    std::string input = std::move(input_queue_.front());
     input_queue_.pop();
     return input;
   }
@@ -90,19 +97,39 @@ class NonBlockingInput {
  private:
   void InputLoop() {
     std::string line;
+    while (true) {
+      fd_set rfds;
+      FD_ZERO(&rfds);
+      FD_SET(pipefd_[0], &rfds);
+      FD_SET(STDIN_FILENO, &rfds);
+      int nfds = std::max(pipefd_[0], STDIN_FILENO) + 1;
 
-    while (std::getline(std::cin, line)) {
-      {
-        std::lock_guard<std::mutex> lock(input_queue_mutex_);
-        input_queue_.push(line);
+      // block until stdin or pipe has data
+      int ret = select(nfds, &rfds, nullptr, nullptr, nullptr);
+      if (ret <= 0) {
+        break;  // error or interrupted
       }
-      input_queue_cv_.notify_one();
-      if (exit_flag_.load()) break;
+      // if pipe has data, that's our exit notification
+      if (FD_ISSET(pipefd_[0], &rfds)) {
+        break;
+      }
+      // if stdin has data, read one line
+      if (FD_ISSET(STDIN_FILENO, &rfds)) {
+        if (!std::getline(std::cin, line)) {
+          break;  // EOF or error
+        }
+        {
+          std::lock_guard<std::mutex> lock(input_queue_mutex_);
+          input_queue_.push(line);
+        }
+        input_queue_cv_.notify_one();
+      }
     }
-    // notify all waiting cv to avoid blocking forever
+    // wake up any waiting PopFirst()
     input_queue_cv_.notify_all();
   }
 
+  int pipefd_[2];
   std::thread input_thread_;
   std::atomic<bool> exit_flag_;
   std::mutex input_queue_mutex_;
